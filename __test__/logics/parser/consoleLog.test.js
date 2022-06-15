b36ee369dca784982130039ae0aa260742bb5b7d
NixSey <106969535+NixSey@users.noreply.github.com>
2022-06-15 09:33:40 +0700

    Update consoleLog.test.js

--- __test__/logics/parser/consoleLog.test.js
+++ __test__/logics/parser/consoleLog.test.js
@@ -3,26 +3,26 @@ const getJsFormat = require('../test-parser-helper')
 
 describe('Test Console log', () => {
   it('Should parse string correctly', () => {
-    const test1 = consoleLog(`spill "Hello world"`)
+    const test1 = consoleLog(`satirin "Hello world"`)
     expect(test1.exp).toBe(`console.log("Hello world");`)
   })
 
   it('Should parse number correctly', () => {
-    const test1 = consoleLog("spill 123")
+    const test1 = consoleLog("satirin 123")
     expect(test1.exp).toBe(`console.log(123);`)
   })
 
   it('Should return null if not match', () =>{
-    const test1 = consoleLog("spil 123")
-    const test2 = consoleLog("spilll false")
+    const test1 = consoleLog("stir 123")
+    const test2 = consoleLog("satirrr false")
     expect(test1).toBe(null)
     expect(test2).toBe(null)
   })
 
   it('Should return correctly flexing', () => {
     const jsFormat = getJsFormat(`
-      literally foo itu "Hello world"
-      spill foo
+      aseli foo itu "Hello world"
+      satirin foo
     `)
     expect(jsFormat).not.toBeNull()
     expect(jsFormat).toContain(`let foo = "Hello world";`)
