5533ea48ebbb55a9c74f3fe781657ba81d5ba293
NixSey <106969535+NixSey@users.noreply.github.com>
2022-06-15 09:39:19 +0700

    Update varReassign.test.js

--- __test__/logics/parser/varReassign.test.js
+++ __test__/logics/parser/varReassign.test.js
@@ -3,26 +3,26 @@ const getJsFormat = require('../test-parser-helper')
 
 describe('Test var Reassign', () => {
   it('Should parse string correctly', () => {
-    const test1 = varReassign("whichis foo itu 'foo'")
+    const test1 = varReassign("beneran foo itu 'foo'")
     expect(test1.exp).toBe(`foo = 'foo';`)
   })
 
   it('Should parse number correctly', () => {
-    const test1 = varReassign("whichis foo itu 123")
+    const test1 = varReassign("beneran foo itu 123")
     expect(test1.exp).toBe(`foo = 123;`)
   })
 
   it('Should return null if not match', () =>{
-    const test1 = varReassign("which foo itu 123")
-    const test2 = varReassign("whi foo i 123")
+    const test1 = varReassign("bener foo itu 123")
+    const test2 = varReassign("bnr foo i 123")
     expect(test1).toBe(null)
     expect(test2).toBe(null)
   })
 
   it('Should return correctly flexing', () => {
     const jsFormat = getJsFormat(`
-    literally foo itu "Hello world"
-    whichis foo itu "Changed"
+    dek foo itu "Hello world"
+    beneran foo itu "Changed"
   `)
     expect(jsFormat).not.toBeNull()
     expect(jsFormat).toContain(`let foo = "Hello world";`)
