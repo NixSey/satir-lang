67c2bd539c488d5f67855fb4d01f815ab339a360
NixSey <106969535+NixSey@users.noreply.github.com>
2022-06-15 09:35:36 +0700

    Update constAssign.test.js

--- __test__/logics/parser/constAssign.test.js
+++ __test__/logics/parser/constAssign.test.js
@@ -3,26 +3,26 @@ const getJsFormat = require('../test-parser-helper')
 
 describe('Test Const assign', () => {
   it('Should parse string correctly', () => {
-    const test1 = constAssign("seriously foo itu 'bar'")
+    const test1 = constAssign("yg_bener foo itu 'bar'")
     expect(test1.exp).toBe(`const foo = 'bar';`)
   })
 
   it('Should parse number correctly', () => {
-    const test1 = constAssign("seriously foo itu 123")
+    const test1 = constAssign("yg_bener foo itu 123")
     expect(test1.exp).toBe(`const foo = 123;`)
   })
 
   it('Should return null if not match', () =>{
-    const test1 = constAssign("serisly foo tu 123")
-    const test2 = constAssign("seriouly foo tu 123")
+    const test1 = constAssign("bener foo tu 123")
+    const test2 = constAssign("yg_bener foo tu 123")
     expect(test1).toBe(null)
     expect(test2).toBe(null)
   })
 
   it('Should return correctly flexing', () => {
     const jsFormat = getJsFormat(`
-      seriously foo itu 123
-      spill foo
+      yg_bener foo itu 123
+      satirin foo
     `)
     expect(jsFormat).not.toBeNull()
     let shouldMatch = [
