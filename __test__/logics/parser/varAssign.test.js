a7f42686957a42c9bfd8c9887ff5bd8153a7d7b5
NixSey <106969535+NixSey@users.noreply.github.com>
2022-06-15 09:37:33 +0700

    Update varAssign.test.js

--- __test__/logics/parser/varAssign.test.js
+++ __test__/logics/parser/varAssign.test.js
@@ -3,26 +3,26 @@ const getJsFormat = require('../test-parser-helper')
 
 describe('Test var assign', () => {
   it('Should parse string correctly', () => {
-    const test1 = varAssign("literally foo itu 'foo'")
+    const test1 = varAssign("dek foo itu 'foo'")
     expect(test1.exp).toBe(`let foo = 'foo';`)
   })
 
   it('Should parse number correctly', () => {
-    const test1 = varAssign("literally foo itu 123")
+    const test1 = varAssign("dek foo itu 123")
     expect(test1.exp).toBe(`let foo = 123;`)
   })
 
   it('Should return null if not match', () =>{
-    const test1 = varAssign("lit foo itu 123")
-    const test2 = varAssign("litetrayl foo i 123")
+    const test1 = varAssign("dik foo itu 123")
+    const test2 = varAssign("kids foo i 123")
     expect(test1).toBe(null)
     expect(test2).toBe(null)
   })
 
   it('Should return correctly flexing', () => {
     const jsFormat = getJsFormat(`
-      literally foo itu 123
-      literally bar itu "test"
+      dek foo itu 123
+      dek bar itu "test"
     `)
     expect(jsFormat).not.toBeNull()
     expect(jsFormat).toContain(`let foo = 123;`)
